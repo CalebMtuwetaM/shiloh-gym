e6dae9f673c8b1b89ef90acd34abb7858c13f315
Caleb <73331147+westillest@users.noreply.github.com>
2023-04-14 17:32:35 +0000

    Basic website setup

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask,render_template,flash,redirect,url_for
+from forms import RegistrationForm,LoginForm
+from flask_wtf.csrf import CSRFProtect
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+  return render_template('home.html')
+
+
+app.config['SECRET_KEY'] = 'Shiloh-gym' # Set your secret key here
+csrf = CSRFProtect(app)
+
+
+@app.route("/register", methods=['GET','POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f"Account created for {form.username.data}!","success")
+        return redirect(url_for("home"))
+    return render_template('register.html', title='Register', form=form)
+    
+
+@app.route("/login", methods=['GET','POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login unsuccessful. Please check username and password','danger')
+    return render_template("login.html",title="Login",form=form)
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0',debug=True)
